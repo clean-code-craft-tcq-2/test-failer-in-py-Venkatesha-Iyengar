20f0c98c70f6f4818f00b00b73fcc6a8b331641c
Venkatesha-Iyengar <90900979+Venkatesha-Iyengar@users.noreply.github.com>
2022-01-25 19:39:41 +0530

    Added Tests to fail the scripts

--- alerter.py
+++ alerter.py
@@ -1,6 +1,9 @@
-alert_failure_count = 0
+import test_alerter
 
-def network_alert_stub(celcius):
+alert_failure_count = 0
+flag_run_from_main_code= False
+
+def network_alert_temporary_stub(celcius):
     print(f'ALERT: Temperature is {celcius} celcius')
     # Return 200 for ok
     # Return 500 for not-ok
@@ -9,7 +12,13 @@ def network_alert_stub(celcius):
 
 def alert_in_celcius(farenheit):
     celcius = (farenheit - 32) * 5 / 9
-    returnCode = network_alert_stub(celcius)
+    if flag_run_from_main_code:
+        returnCode = network_alert_temporary_stub(celcius)
+
+    else:
+        #Running from some other script(Test-Script)
+        returnCode = test_alerter.network_alert_stub(celcius)
+        
     if returnCode != 200:
         # non-ok response is not an error! Issues happen in life!
         # let us keep a count of failures to report
@@ -18,8 +27,10 @@ def alert_in_celcius(farenheit):
         global alert_failure_count
         alert_failure_count += 0
 
-
-alert_in_celcius(400.5)
-alert_in_celcius(303.6)
-print(f'{alert_failure_count} alerts failed.')
-print('All is well (maybe!)')
+if __name__ == '__main__':
+    flag_run_from_main_code = True
+    alert_in_celcius(400.5)
+    alert_in_celcius(303.6)
+    print(f'{alert_failure_count} alerts failed.')
+    assert(alert_failure_count>0)
+    print('All is well (maybe!)')
--- misaligned.py
+++ misaligned.py
@@ -1,13 +1,38 @@
-
 def print_color_map():
+    list_color_pairs_console_print = list()
     major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
     minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
     for i, major in enumerate(major_colors):
         for j, minor in enumerate(minor_colors):
             print(f'{i * 5 + j} | {major} | {minor}')
-    return len(major_colors) * len(minor_colors)
+            list_color_pairs_console_print.append(f'{i * 5 + j} | {major} | {minor}')
+    return len(major_colors) * len(minor_colors), list_color_pairs_console_print
 
+def test_ColorMap_numerical_values(list_color_pairs_console_print):
+    #Lets Calculate the length of numerical seperation for all elements in list
+    major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
+    minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
+    list_received_color_pairs_numerical_values  = [int(color_pair_print.split('|')[0].strip()) for color_pair_print in list_color_pairs_console_print]
+    
+    list_actual_color_pairs_numerical_values = [(i * 5 + j)+1 for i in range(len(major_colors)) for j in range(len(minor_colors))]
+    return list_received_color_pairs_numerical_values, list_actual_color_pairs_numerical_values
 
-result = print_color_map()
+def test_ColorMap_seperator_allignment(list_color_pairs_console_print):
+    #Lets Calculate the length of numerical seperation for all elements in list
+    list_color_pairs_seperation_values  = [len(color_pair_print.split('|')[0]) for color_pair_print in list_color_pairs_console_print]
+
+    #We will return the length of the set(unique values) of the list.
+    return len(set(list_color_pairs_seperation_values))
+    
+result, list_color_pairs_console_print = print_color_map()
 assert(result == 25)
+
+list_received_color_pairs_numerical_values, list_actual_color_pairs_numerical_values = test_ColorMap_numerical_values(list_color_pairs_console_print)
+number_of_different_seperations = test_ColorMap_seperator_allignment(list_color_pairs_console_print)
+
+assert(list_received_color_pairs_numerical_values == list_actual_color_pairs_numerical_values)
+
+#We will assert if the return value is 1 which means all the seperation values are same. Then they are of same length
+assert(number_of_different_seperations == 1) 
+
 print("All is well (maybe!)\n")
